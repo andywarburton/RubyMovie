70a5d43ad3dd3a0f1868708160b706858d67b05f
Andy Warburton <andy@stinkyink.com>
2011-04-17 10:37:41 +0100

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyMovie::Application.config.secret_token = '8dc4840fa736e8fecadce71e5349cf2fa9b01f4ba52a0eefcb9d2bdd2342ffe260e206855cb6649598fc562c78162ca3a92e23ef4120531b2d27e4951779486b'
